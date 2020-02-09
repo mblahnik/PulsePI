58bc44b7ebc0199216b12136ff5838434c3b9a95
Alex <mblahnik@uwm.edu>
2020-02-09 14:51:14 -0600

    Add project files.

---
+++ PulsePI/Controllers/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PulsePI.Controllers
+{
+    public class Person
+    {
+        public Person(string x, int y)
+        {
+            name = x;
+            age = y;
+        }
+
+
+        string name { get; set; }
+        int age { get; set; }
+
+    }
+}
---
+++ PulsePI/Controllers/TestControler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PulsePI.Controllers
+{
+    public class TestControler : Controller
+    {
+        [HttpGet("api/whatever")]
+        public IEnumerable<Person> getWhatever()
+        {
+            return new List<Person> { new Person("Bob",5) , new Person("Steve", 4)};
+        }
+
+    }
+}
