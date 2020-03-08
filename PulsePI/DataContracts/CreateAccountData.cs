cb0c30807ea2f500b8515de02ef2944802a2b485
Leuenberger <leuenbe4@uwm.edu>
2020-03-08 14:31:09 -0500

    Starting create accnt

---
+++ PulsePI/DataContracts/CreateAccountData.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PulsePI.DataContracts
+{
+    public class CreateAccountData
+    {
+        public string password { get; set; }
+        public string username { get; set; }
+    }
+}
