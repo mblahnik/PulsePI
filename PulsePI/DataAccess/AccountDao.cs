e241acc794334095e9f86779009e6c52238e9a53
Leuenberger <leuenbe4@uwm.edu>
2020-04-05 19:18:43 -0500

    Fixing connection string

--- PulsePI/DataAccess/AccountDao.cs
+++ PulsePI/DataAccess/AccountDao.cs
@@ -6,6 +6,7 @@ using PulsePI.MessageContracts;
 using PulsePI.Models;
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace PulsePI.DataAccess
@@ -13,64 +14,61 @@ namespace PulsePI.DataAccess
     public class AccountDao : IAccountDao
     {
         private readonly IConfiguration _config;
+        private PulsePiDBContext _context;
 
-        public AccountDao(IConfiguration config)
+        public AccountDao(IConfiguration config, PulsePiDBContext context)
         {
-            _config = config; 
+            _config = config;
+            _context = context;
         }
 
         public async Task<LoginMessage> Login(string username, string password)
         {
-            using(var context = new PulsePiDBContext())
+            Account acc;
+            try
             {
-                Account acc;
-                try
-                {
-                    acc = await context.accounts.Where(x => (x.username == username) &&
-                    (x.password == password)).FirstOrDefaultAsync();
-                }
-                catch (Exception e)
-                {
-                    throw new CustomException("Database error at login", e);
-                }
-
-                if (acc == null) throw new CustomException("Account not found");
-
-                return new LoginMessage(acc.username, acc.firstName, acc.lastName,
-                    acc.middleName, acc.birthDate, acc.avatarUrl, acc.email);
+                acc = await _context.accounts.Where(x => (x.username == username) &&
+                (x.password == password)).FirstOrDefaultAsync();
             }
-  
+            catch (Exception e)
+            {
+                throw new CustomException("Database error at login", e);
+            }
+
+            if (acc == null) throw new CustomException("Account not found");
+
+            return new LoginMessage(acc.username, acc.firstName, acc.lastName,
+                acc.middleName, acc.birthDate, acc.avatarUrl, acc.email);
         }
 
         public async Task CreateAccount(string u, string p, string f, string l, string e)
         {
             string defaultUrl = _config["DefaultAvatarUrl"] ?? throw new Exception("No Default avatar url in appsettings");
-            using(var context = new PulsePiDBContext())
+
+            Account acc = await _context.accounts.Where(x => x.username == u).FirstOrDefaultAsync();
+            if (acc != null) throw new CustomException("Account already exists");
+
+            acc = new Account()
             {
-                Account acc = await context.accounts.Where(x => x.username == u).FirstOrDefaultAsync();
-                if (acc != null) throw new CustomException("Account already exists");
+                username = u,
+                password = p,
+                firstName = f,
+                lastName = l,
+                email = e,
+                avatarUrl = defaultUrl
+            };
 
-                acc = new Account()
-                {
-                    username = u,
-                    password = p,
-                    firstName = f,
-                    lastName = l,
-                    email = e,
-                    avatarUrl = defaultUrl
-                };
-
-                try
-                {
-                    context.accounts.Add(acc);
-                    context.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    throw new CustomException("Error creating account", ex);
-                }    
+            try
+            {
+                _context.accounts.Add(acc);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException("Error creating account", ex);
             }
         }
-
     }
 }
+
+
