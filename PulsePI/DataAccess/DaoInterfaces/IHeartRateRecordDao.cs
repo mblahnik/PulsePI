4d1af2a722d20f6003f6fe96a9eb11bb742a9b5e
Leuenberger <leuenbe4@uwm.edu>
2020-04-13 19:29:04 -0500

    new endpoints to get stuff

--- PulsePI/DataAccess/DaoInterfaces/IHeartRateRecordDao.cs
+++ PulsePI/DataAccess/DaoInterfaces/IHeartRateRecordDao.cs
@@ -9,6 +9,8 @@ namespace PulsePI.DataAccess.DaoInterfaces
     public interface IHeartRateRecordDao
     {
         Task RecordHeartRate(HeartRateRecord hr, string u);
-        Task<List<GetAllHRDataMessage>> GetAllHeartRateData(GetAllHRData hrd);
+        Task<List<GetAllHRDataMessage>> GetAllHeartRateData(GetHRData hrd);
+        Task<List<GetRestingHeartRateMsg>> GetRestingHeartRateHistory(GetHRData hr);
+        Task<List<GetExerciseHeartRateMsg>> GetExerciseHeartRateHistory(GetHRData hr);
     }
 }
---
+++ PulsePI/MessageContracts/GetExerciseHeartRateMsg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PulsePI.MessageContracts
+{
+    public class GetExerciseHeartRateMsg
+    {
+        public string type { get; set; }
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public double bpmLow { get; set; }
+        public double bpmHigh { get; set; }
+        public double bpmAvg { get; set; }
+    }
+}
