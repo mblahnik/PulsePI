976c5f537743c3215835976e4f0846a9cecce180
spykim2003 <spykim2003@gmail.com>
2020-04-23 03:13:39 -0500

    biometric service test

---
+++ PulseTest/ServiceTests/BiometricServiceTests.cs
@@ -0,0 +1,37 @@
+using System;
+using Moq;
+using PulsePI.DataContracts;
+using FluentAssertions;
+using PulsePI.Service.ServiceInterfaces;
+using Xunit;
+
+namespace PulseTest.ServiceTests
+{
+    public class BiometricServiceTests
+    {
+        private Mock<IBiometricService> mockBiometricService = new Mock<IBiometricService>();
+
+        [Fact]
+        public void BiometricService_CreateBiometric_Test()
+        {
+            //arrange 
+            var biometricData = new BiometricData()
+            {
+                username = "jack",
+                height = "6.0",
+                weight = "152",
+                sex = "M",
+             dob = "1995/04/23"
+            };
+
+            //act 
+            var response = mockBiometricService.Object.CreateBiometric(biometricData);
+
+            //assert
+            Assert.True(response.IsCompletedSuccessfully);
+            mockBiometricService.Verify();
+        }
+
+    }
+
+}
